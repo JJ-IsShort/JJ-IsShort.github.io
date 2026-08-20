3fba2accbd18a77cab7e68358d1d1f2693e6aad5
JJ-IsShort <crazygamer.js73@gmail.com>
2026-08-20 18:51:28 -0400

    New colour schemes

--- src/main/styling.cljs
+++ src/main/styling.cljs
@@ -10,7 +10,22 @@
     :highlight "#8540c9"}
    {:base "#fafafa"
     :text "#020202"
-    :highlight "#020202"}])
+    :highlight "#020202"}
+   {:base "#24292E"
+    :text "#A9B9CE"
+    :highlight "#A35C50"}
+   {:base "#1F1F1F" ; Colour schemes past this point inspired by the Intent Editor
+    :text "#E9E5E5"
+    :highlight "#FFE4C4"}
+   {:base "#FEF8EB"
+    :text "#222222"
+    :highlight "#B4B4B4"}
+   {:base "#282C34"
+    :text "#E9E5E5"
+    :highlight "#4F4F4F"}
+   {:base "#F7EEE8"
+    :text "#000000"
+    :highlight "#EC76AB"}])
 
 (defn set_colors [index]
   (let [computed-style (.-style (.querySelector js/document ":root"))
